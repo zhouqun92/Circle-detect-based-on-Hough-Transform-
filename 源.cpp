132fa2baf28d0d7f41a0464aa38452fde5aa8b50
zhouqun92 <15280067611@163.com>
2019-10-21 20:11:25 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,22 @@
+#include "findcircle.h"
+//The core function:imfindcircles
+//Adjustable parameter
+//src:input img 
+//rmin:Minimum radius
+//rmax:Maximum radius
+//sensitivity:Larger is easier to detect a circle ,range(0-1)
+//edgeThresh:The gradient threshold,range(0-1)
+void main()
+{
+	Mat img = imread("2.jpg");
+	float center_x, center_y, center_r;
+	DWORD timestart = GetTickCount();
+	findcircle(img, center_x, center_y, center_r);
+	DWORD timeend = GetTickCount();
+	cout<< "cost time:" << timeend - timestart << endl;
+	circle(img, Point(center_x, center_y), center_r, Scalar(0, 0, 255));
+	namedWindow("result",0);
+	imshow("result", img); waitKey(0);
+	imwrite("result.png", img);
+	//system("pause");
+}
